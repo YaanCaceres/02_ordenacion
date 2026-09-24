3083e38bb92c982e5ccaf2f08049be0a5d6e1d15
YaanCaceres <ycaceresl@unjbg.edu.pe>
2026-09-24 16:59:48 -0500

    2da version metodo rapido

--- Metodo rapido.cpp
+++ Metodo rapido.cpp
@@ -1,4 +1,15 @@
 //Metodo rapido
 #include <iostream>
 using namespace std; 
-
+void QuickSort(int A[],int n){
+	Reduce( A[],1,n);
+}
+void Reduce(int A[],int inicio, int final){
+	int izq= inicio;
+	int der= final;
+	int pos=izq;
+	int cen=1;
+	while (cen=1){
+		while(A[pos] =< A[der] && pos!= )
+	}
+}
